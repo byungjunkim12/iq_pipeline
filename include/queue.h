e776cfd1c9e5aa8151806a4592510aeb62b3f0a6
byungjunkim12 <byungjunkim12@gmail.com>
2026-03-05 13:59:42 -0500

    feat: implement circular buffer for FIFO

--- include/queue.h
+++ include/queue.h
@@ -13,9 +13,12 @@ private:
     std::mutex mutex_;
     std::condition_variable cv_;
     bool done_ = false;
+    
+    size_t head_;
+    size_t tail_;
 
 public:
-    IQQueue(size_t capacity) : capacity_(capacity), size_(0) { buffer_ = std::make_unique<T[]>(capacity_); }
+    IQQueue(size_t capacity) : capacity_(capacity), size_(0), head_(0), tail_(0) { buffer_ = std::make_unique<T[]>(capacity_); }
     
     IQQueue(IQQueue&& other) noexcept : 
         capacity_(other.capacity_),
@@ -28,14 +31,18 @@ public:
     void push(const T& item) {
         std::unique_lock<std::mutex> lock(mutex_);
         cv_.wait(lock, [this] { return size_ < capacity_; });
-        buffer_[size_++] = item;
+        buffer_[tail_] = item;
+        tail_ = (tail_ + 1) % capacity_;
+        size_++;
         cv_.notify_one();
     }
 
     void push(T&& item) {
         std::unique_lock<std::mutex> lock(mutex_);
         cv_.wait(lock, [this] { return size_ < capacity_; });
-        buffer_[size_++] = std::move(item);
+        buffer_[tail_] = std::move(item);
+        tail_ = (tail_ + 1) % capacity_;
+        size_++;
         cv_.notify_one();
     }
 
@@ -43,7 +50,9 @@ public:
         std::unique_lock<std::mutex> lock(mutex_);
         cv_.wait(lock, [this] { return size_ > 0 || done_; });
         if (empty()) return T{};
-        T item = buffer_[--size_];
+        T item = buffer_[head_];
+        head_ = (head_ + 1) % capacity_;
+        size_--;
         cv_.notify_one();
         return item;
     }
